2983341ff14d17f274ed6ad260682bdc8099e9f9
unknown <arpadbacos@gmail.com>
2023-07-08 19:50:32 +0300

    wip

--- src/app/modules/auth/core/services/auth.service.ts
+++ src/app/modules/auth/core/services/auth.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+	Auth,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	User as FirebaseUser,
+	UserCredential,
+} from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private fireAuth: Auth, private router: Router) {}
 
-  constructor() { }
+  login(email: string, password: string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.fireAuth, email, password);
+  }
+
+  getCurrentUse(): FirebaseUser | null {
+    return this.fireAuth.currentUser;
+  }
+
+  register(email: string, password: string): Promise<UserCredential> {
+    return createUserWithEmailAndPassword(this.fireAuth, email, password);
+  }
+
+  async logout() {
+    await this.fireAuth.signOut();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
         path: 'home',
 				component: HomeComponent,
       },
+			{ path: '**', redirectTo: '/home', pathMatch: 'full' },
     ],
   },
 ];
