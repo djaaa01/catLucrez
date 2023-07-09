afe1826c3eafee19500da2558f38e1809e8452f5
unknown <arpadbacos@gmail.com>
2023-07-09 13:43:03 +0300

    add header

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   email: string;
   password: string;
+  isLoading =  false;
 
   constructor(
     private readonly authService: AuthService,
@@ -19,7 +20,9 @@ export class LoginComponent {
 	@HostListener('window:keyup.enter', ['$event', 'undefined'])
 	onLogin(): void {
 		if (this.email && this.password) {
+      this.isLoading = true;
 			this.authService.login(this.email, this.password).then(() => {
+        this.isLoading = false;
 				this.router.navigate(['/home']);
 			});
 		}
